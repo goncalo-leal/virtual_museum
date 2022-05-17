1e3d67da7796fd37ecfbbcc2ae240112736b65eb
goncalo-leal <goncalolealsilva@ua.pt>
2022-05-17 03:45:42 +0100

    code refactor

---
+++ configs/paintings.js
@@ -0,0 +1,488 @@
+// wall_row_column
+
+const paintings = {
+    "0_3_0": {
+        "picture": "./assets/numbers/um-01.jpg",
+        "row": 3
+    },
+    "0_2_3": {
+        "picture": "./assets/paintings/1-01.jpg",
+        "row": 2
+    },
+    "0_2_4": {
+        "picture": "./assets/paintings/1-02.jpg",
+        "row": 2
+    },
+    "0_2_5": {
+        "picture": "./assets/paintings/1-03.jpg",
+        "row": 2
+    },
+    "0_2_6": {
+        "picture": "./assets/paintings/1-04.jpg",
+        "row": 2
+    },
+    "0_1_3": {
+        "picture": "./assets/paintings/1-05.jpg",
+        "row": 1
+    },
+    "0_1_4": {
+        "picture": "./assets/paintings/1-06.jpg",
+        "row": 1
+    },
+    "0_1_5": {
+        "picture": "./assets/paintings/1-07.jpg",
+        "row": 1
+    },
+    "0_1_6": {
+        "picture": "./assets/paintings/1-08.jpg",
+        "row": 1
+    },
+
+    "0_2_10": {
+        "picture": "./assets/paintings/quadro_partido2-09.png",
+        "row": 2
+    },
+    "0_2_11": {
+        "picture": "./assets/paintings/quadro_partido2-10.png",
+        "row": 2
+    },
+    "0_2_12": {
+        "picture": "./assets/paintings/quadro_partido2-11.png",
+        "row": 2
+    },
+    "0_2_13": {
+        "picture": "./assets/paintings/quadro_partido2-12.png",
+        "row": 2
+    },
+    "0_1_10": {
+        "picture": "./assets/paintings/quadro_partido2-13.png",
+        "row": 1
+    },
+    "0_1_11": {
+        "picture": "./assets/paintings/quadro_partido2-14.png",
+        "row": 1
+    },
+    "0_1_12": {
+        "picture": "./assets/paintings/quadro_partido2-15.png",
+        "row": 1
+    },
+    "0_1_13": {
+        "picture": "./assets/paintings/quadro_partido2-16.png",
+        "row": 1
+    },
+
+    "0_2_17": {
+        "picture": "./assets/paintings/we-09.png",
+        "row": 2
+    },
+    "0_2_18": {
+        "picture": "./assets/paintings/we-10.png",
+        "row": 2
+    },
+    "0_2_19": {
+        "picture": "./assets/paintings/we-11.png",
+        "row": 2
+    },
+    "0_2_20": {
+        "picture": "./assets/paintings/we-12.png",
+        "row": 2
+    },
+    "0_1_17": {
+        "picture": "./assets/paintings/we-13.png",
+        "row": 1
+    },
+    "0_1_18": {
+        "picture": "./assets/paintings/we-14.png",
+        "row": 1
+    },
+    "0_1_19": {
+        "picture": "./assets/paintings/we-15.png",
+        "row": 1
+    },
+    "0_1_20": {
+        "picture": "./assets/paintings/we-16.png",
+        "row": 1
+    },
+
+    "0_2_24": {
+        "picture": "./assets/paintings/ri-12.jpg",
+        "row": 2
+    },
+    "0_2_25": {
+        "picture": "./assets/paintings/ri-14.jpg",
+        "row": 2
+    },
+    "0_1_24": {
+        "picture": "./assets/paintings/ri-13.jpg",
+        "row": 1
+    },
+    "0_1_25": {
+        "picture": "./assets/paintings/ri-15.jpg",
+        "row": 1
+    },
+
+
+    "2_3_27": {
+        "picture": "./assets/paintings/numbers-05.jpg",
+        "row": 3
+    },
+    "2_2_25": {
+        "picture": "./assets/paintings/borboletas-09.jpg",
+        "row": 2
+    },
+    "2_2_24": {
+        "picture": "./assets/paintings/borboletas-10.jpg",
+        "row": 2
+    },
+    "2_2_23": {
+        "picture": "./assets/paintings/borboletas-11.jpg",
+        "row": 2
+    },
+    "2_2_22": {
+        "picture": "./assets/paintings/borboletas-12.jpg",
+        "row": 2
+    },
+    "2_1_25": {
+        "picture": "./assets/paintings/borboletas-13.jpg",
+        "row": 1
+    },
+    "2_1_24": {
+        "picture": "./assets/paintings/borboletas-14.jpg",
+        "row": 1
+    },
+    "2_1_23": {
+        "picture": "./assets/paintings/borboletas-15.jpg",
+        "row": 1
+    },
+    "2_1_22": {
+        "picture": "./assets/paintings/borboletas-16.jpg",
+        "row": 1
+    },
+
+    "2_2_18": {
+        "picture": "./assets/paintings/borboletas_2-05.jpg",
+        "row": 2
+    },
+    "2_2_17": {
+        "picture": "./assets/paintings/borboletas_2-06.jpg",
+        "row": 2
+    },
+    "2_2_16": {
+        "picture": "./assets/paintings/borboletas_2-07.jpg",
+        "row": 2
+    },
+    "2_2_15": {
+        "picture": "./assets/paintings/borboletas_2-08.jpg",
+        "row": 2
+    },
+    "2_1_18": {
+        "picture": "./assets/paintings/borboletas_2-09.jpg",
+        "row": 1
+    },
+    "2_1_17": {
+        "picture": "./assets/paintings/borboletas_2-10.jpg",
+        "row": 1
+    },
+    "2_1_16": {
+        "picture": "./assets/paintings/borboletas_2-11.jpg",
+        "row": 1
+    },
+    "2_1_15": {
+        "picture": "./assets/paintings/borboletas_2-12.jpg",
+        "row": 1
+    },
+
+    "2_2_11": {
+        "picture": "./assets/paintings/borboletas_3-05.jpg",
+        "row": 2
+    },
+    "2_2_10": {
+        "picture": "./assets/paintings/borboletas_3-06.jpg",
+        "row": 2
+    },
+    "2_2_9": {
+        "picture": "./assets/paintings/borboletas_3-07.jpg",
+        "row": 2
+    },
+    "2_2_8": {
+        "picture": "./assets/paintings/borboletas_3-08.jpg",
+        "row": 2
+    },
+    "2_1_11": {
+        "picture": "./assets/paintings/borboletas_3-09.jpg",
+        "row": 1
+    },
+    "2_1_10": {
+        "picture": "./assets/paintings/borboletas_3-10.jpg",
+        "row": 1
+    },
+    "2_1_9": {
+        "picture": "./assets/paintings/borboletas_3-11.jpg",
+        "row": 1
+    },
+    "2_1_8": {
+        "picture": "./assets/paintings/borboletas_3-12.jpg",
+        "row": 1
+    },
+
+    "2_2_4": {
+        "picture": "./assets/paintings/borboleta_main-03.jpg",
+        "row": 2
+    },
+    "2_2_3": {
+        "picture": "./assets/paintings/borboleta_main-04.jpg",
+        "row": 2
+    },
+    "2_1_4": {
+        "picture": "./assets/paintings/borboleta_main-05.jpg",
+        "row": 1
+    },
+    "2_1_3": {
+        "picture": "./assets/paintings/borboleta_main-06.jpg",
+        "row": 1
+    },
+
+
+    "1_3_27": {
+        "picture": "./assets/paintings/numbers-03.jpg",
+        "row": 3
+    },
+    "1_2_25": {
+        "picture": "./assets/paintings/naosei-05.jpg",
+        "row": 2
+    },
+    "1_2_24": {
+        "picture": "./assets/paintings/naosei-06.jpg",
+        "row": 2
+    },
+    "1_2_23": {
+        "picture": "./assets/paintings/naosei-07.jpg",
+        "row": 2
+    },
+    "1_2_22": {
+        "picture": "./assets/paintings/naosei-08.jpg",
+        "row": 2
+    },
+    "1_1_25": {
+        "picture": "./assets/paintings/naosei-09.jpg",
+        "row": 1
+    },
+    "1_1_24": {
+        "picture": "./assets/paintings/naosei-10.jpg",
+        "row": 1
+    },
+    "1_1_23": {
+        "picture": "./assets/paintings/naosei-11.jpg",
+        "row": 1
+    },
+    "1_1_22": {
+        "picture": "./assets/paintings/naosei-12.jpg",
+        "row": 1
+    },
+
+    "1_2_18": {
+        "picture": "./assets/paintings/naosei_2-05.jpg",
+        "row": 2
+    },
+    "1_2_17": {
+        "picture": "./assets/paintings/naosei_2-06.jpg",
+        "row": 2
+    },
+    "1_2_16": {
+        "picture": "./assets/paintings/naosei_2-07.jpg",
+        "row": 2
+    },
+    "1_2_15": {
+        "picture": "./assets/paintings/naosei_2-08.jpg",
+        "row": 2
+    },
+    "1_1_18": {
+        "picture": "./assets/paintings/naosei_2-09.jpg",
+        "row": 1
+    },
+    "1_1_17": {
+        "picture": "./assets/paintings/naosei_2-10.jpg",
+        "row": 1
+    },
+    "1_1_16": {
+        "picture": "./assets/paintings/naosei_2-11.jpg",
+        "row": 1
+    },
+    "1_1_15": {
+        "picture": "./assets/paintings/naosei_2-12.jpg",
+        "row": 1
+    },
+
+    "1_2_11": {
+        "picture": "./assets/paintings/naosei_3-05.jpg",
+        "row": 2
+    },
+    "1_2_10": {
+        "picture": "./assets/paintings/naosei_3-06.jpg",
+        "row": 2
+    },
+    "1_2_9": {
+        "picture": "./assets/paintings/naosei_3-07.jpg",
+        "row": 2
+    },
+    "1_2_8": {
+        "picture": "./assets/paintings/naosei_3-08.jpg",
+        "row": 2
+    },
+    "1_1_11": {
+        "picture": "./assets/paintings/naosei_3-09.jpg",
+        "row": 1
+    },
+    "1_1_10": {
+        "picture": "./assets/paintings/naosei_3-10.jpg",
+        "row": 1
+    },
+    "1_1_9": {
+        "picture": "./assets/paintings/naosei_3-11.jpg",
+        "row": 1
+    },
+    "1_1_8": {
+        "picture": "./assets/paintings/naosei_3-12.jpg",
+        "row": 1
+    },
+
+    "1_2_4": {
+        "picture": "./assets/paintings/naosei_main-02.jpg",
+        "row": 2
+    },
+    "1_2_3": {
+        "picture": "./assets/paintings/naosei_main-03.jpg",
+        "row": 2
+    },
+    "1_1_4": {
+        "picture": "./assets/paintings/naosei_main-04.jpg",
+        "row": 1
+    },
+    "1_1_3": {
+        "picture": "./assets/paintings/naosei_main-05.jpg",
+        "row": 1
+    },
+
+    
+    "3_3_0": {
+        "picture": "./assets/paintings/numbers-02.jpg",
+        "row": 1
+    },
+    "3_2_3": {
+        "picture": "./assets/paintings/homemdascores-03.jpg",
+        "row": 2
+    },
+    "3_2_4": {
+        "picture": "./assets/paintings/homemdascores-04.jpg",
+        "row": 2
+    },
+    "3_1_3": {
+        "picture": "./assets/paintings/homemdascores-05.jpg",
+        "row": 1
+    },
+    "3_1_4": {
+        "picture": "./assets/paintings/homemdascores-06.jpg",
+        "row": 1
+    },
+    
+    "3_2_8": {
+        "picture": "./assets/paintings/outro-03.jpg",
+        "row": 2
+    },
+    "3_2_9": {
+        "picture": "./assets/paintings/outro-04.jpg",
+        "row": 2
+    },
+    "3_2_10": {
+        "picture": "./assets/paintings/outro-05.jpg",
+        "row": 2
+    },
+    "3_2_11": {
+        "picture": "./assets/paintings/outro-06.jpg",
+        "row": 2
+    },
+    "3_1_8": {
+        "picture": "./assets/paintings/outro-07.jpg",
+        "row": 1
+    },
+    "3_1_9": {
+        "picture": "./assets/paintings/outro-08.jpg",
+        "row": 1
+    },
+    "3_1_10": {
+        "picture": "./assets/paintings/outro-09.jpg",
+        "row": 1
+    },
+    "3_1_11": {
+        "picture": "./assets/paintings/outro-10.jpg",
+        "row": 1
+    },
+
+    "3_3_15": {
+        "picture": "./assets/paintings/blind-07.jpg",
+        "row": 3
+    },
+    "3_3_16": {
+        "picture": "./assets/paintings/blind-08.jpg",
+        "row": 3
+    },
+    "3_3_17": {
+        "picture": "./assets/paintings/blind-09.jpg",
+        "row": 3
+    },
+    "3_2_15": {
+        "picture": "./assets/paintings/blind-10.jpg",
+        "row": 2
+    },
+    "3_2_16": {
+        "picture": "./assets/paintings/blind-11.jpg",
+        "row": 2
+    },
+    "3_2_17": {
+        "picture": "./assets/paintings/blind-12.jpg",
+        "row": 2
+    },
+    "3_1_15": {
+        "picture": "./assets/paintings/blind-13.jpg",
+        "row": 1
+    },
+    "3_1_16": {
+        "picture": "./assets/paintings/blind-14.jpg",
+        "row": 1
+    },
+    "3_1_17": {
+        "picture": "./assets/paintings/blind-15.jpg",
+        "row": 1
+    },
+    "3_0_15": {
+        "picture": "./assets/paintings/blind-16.jpg",
+        "row": 0
+    },
+    "3_0_16": {
+        "picture": "./assets/paintings/blind-17.jpg",
+        "row": 0
+    },
+    "3_0_17": {
+        "picture": "./assets/paintings/blind-18.jpg",
+        "row": 0
+    },
+
+    "3_2_21": {
+        "picture": "./assets/paintings/blind0-15.jpg",
+        "row": 1
+    },
+    "3_2_22": {
+        "picture": "./assets/paintings/blind0-16.jpg",
+        "row": 0
+    },
+    "3_2_23": {
+        "picture": "./assets/paintings/blind0-17.jpg",
+        "row": 0
+    },
+    "3_2_24": {
+        "picture": "./assets/paintings/blind0-18.jpg",
+        "row": 0
+    }
+};
+
+export { paintings };
---
+++ configs/wall_paintings.js
@@ -0,0 +1,152 @@
+const wall_paintings = {
+    '1': [
+        {
+            'image': './assets/paintings/P1070122.JPG',
+            'author': 'Lara Lopes',
+            'desc': 'Pintura tinta a óleo',
+            'size': '60x50cm',
+            'year': '2021',
+            'location': 'Arrezima'
+        },
+        {
+            'image': './assets/paintings/P1070131.JPG',
+            'author': 'Lara Lopes',
+            'desc': 'Pintura tinta a óleo',
+            'size': '60x50cm',
+            'year': '2021',
+            'location': 'Arrezima'
+        },
+        {
+            'image': './assets/paintings/P1070132.JPG',
+            'author': 'Lara Lopes',
+            'desc': 'Pintura tinta a óleo',
+            'size': '60x50cm',
+            'year': '2021',
+            'location': 'Arrezima'
+        },
+        {
+            'image': './assets/paintings/P1070165.JPG',
+            'author': 'Lara Lopes',
+            'desc': 'Pintura tinta a óleo',
+            'size': '60x50cm',
+            'year': '2021',
+            'location': 'Arrezima'
+        },
+        {
+            'image': './assets/paintings/P1070124.JPG',
+            'author': 'Lara Lopes',
+            'desc': 'Pintura tinta a óleo',
+            'size': '60x50cm',
+            'year': '2021',
+            'location': 'Arrezima'
+        },
+    ],
+
+    '2': [
+        {
+            'image': './assets/paintings/colours.jpeg',
+            'author': 'Lara Lopes',
+            'desc': 'Pintura tinta a óleo',
+            'size': '60x50cm',
+            'year': '2021',
+            'location': 'Arrezima'
+        },
+        {
+            'image': './assets/paintings/blind.jpeg',
+            'author': 'Lara Lopes',
+            'desc': 'Pintura tinta a óleo',
+            'size': '60x50cm',
+            'year': '2021',
+            'location': 'Arrezima'
+        },
+    ],
+
+    '3': [
+        {
+            'image': './assets/paintings/naosei_main.JPG',
+            'author': 'Lara Lopes',
+            'desc': 'Pintura tinta a óleo',
+            'size': '60x50cm',
+            'year': '2021',
+            'location': 'Arrezima'
+        },
+        {
+            'image': './assets/paintings/P1070183.JPG',
+            'author': 'Lara Lopes',
+            'desc': 'Pintura tinta a óleo',
+            'size': '60x50cm',
+            'year': '2021',
+            'location': 'Arrezima'
+        },
+        {
+            'image': './assets/paintings/P1070177.JPG',
+            'author': 'Lara Lopes',
+            'desc': 'Pintura tinta a óleo',
+            'size': '60x50cm',
+            'year': '2021',
+            'location': 'Arrezima'
+        },
+        {
+            'image': './assets/paintings/P1070175.JPG',
+            'author': 'Lara Lopes',
+            'desc': 'Pintura tinta a óleo',
+            'size': '60x50cm',
+            'year': '2021',
+            'location': 'Arrezima'
+        },
+        {
+            'image': './assets/paintings/naosei_3-10.jpg',
+            'author': 'Lara Lopes',
+            'desc': 'Pintura tinta a óleo',
+            'size': '60x50cm',
+            'year': '2021',
+            'location': 'Arrezima'
+        },
+    ],
+
+
+    '4': [
+        {
+            'image': './assets/paintings/borboletas_main.jpeg',
+            'author': 'Lara Lopes',
+            'desc': 'Pintura tinta a óleo',
+            'size': '60x50cm',
+            'year': '2021',
+            'location': 'Arrezima'
+        },
+        {
+            'image': './assets/paintings/P1070145.JPG',
+            'author': 'Lara Lopes',
+            'desc': 'Pintura tinta a óleo',
+            'size': '60x50cm',
+            'year': '2021',
+            'location': 'Arrezima'
+        },
+        {
+            'image': './assets/paintings/P1070138.JPG',
+            'author': 'Lara Lopes',
+            'desc': 'Pintura tinta a óleo',
+            'size': '60x50cm',
+            'year': '2021',
+            'location': 'Arrezima'
+        },
+        {
+            'image': './assets/paintings/P1070174.JPG',
+            'author': 'Lara Lopes',
+            'desc': 'Pintura tinta a óleo',
+            'size': '60x50cm',
+            'year': '2021',
+            'location': 'Arrezima'
+        },
+        {
+            'image': './assets/paintings/borboletas_head.jpeg',
+            'author': 'Lara Lopes',
+            'desc': 'Pintura tinta a óleo',
+            'size': '60x50cm',
+            'year': '2021',
+            'location': 'Arrezima'
+        },
+    ]
+};
+
+export { wall_paintings };
